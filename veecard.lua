4cd796f8d5f1bffb7e848fab0b0e6b942fa7c064
REEEPlayz <163062030+REEEPlayz@users.noreply.github.com>
2026-06-01 12:34:44 +0300

    Create veecard.lua

---
+++ veecard.lua
@@ -0,0 +1,92 @@
+writefile("veecard.png", game:HttpGet("https://raw.githubusercontent.com/REEEPlayz/fuzzyengine/refs/heads/main/cards/veecard.png", true))
+local p = game:GetService("Players").LocalPlayer
+local voteFrame = p.PlayerGui.ScreenGui.VoteFrame
+
+local fancy = voteFrame:WaitForChild("FancyTemplate")
+local old = voteFrame:WaitForChild("Template")
+if old then
+old:Destroy()
+end
+
+local clone = fancy:Clone()
+
+for _, obj in ipairs(clone:GetDescendants()) do
+    if obj:IsA("Script") or obj:IsA("LocalScript") then
+        obj:Destroy()
+    end
+end
+
+clone.Name = "Template"
+
+local holder = clone.Holder
+
+holder.ItemDescrption.Position = UDim2.new(0.25, 0, 0.430000007, 0)
+holder.ItemDescrption.Size     = UDim2.new(0.5, 0, 0.300000012, 0)
+holder.ItemDescrption.TextStrokeTransparency = 0
+
+holder.ItemName.Position = UDim2.new(0.209999993, 0, -0.00165538606, 0)
+holder.ItemName.Size     = UDim2.new(0.600000024, 0, 0.120507024, 0)
+
+holder.ItemImage.Position = UDim2.new(0.209999993, 0, 0.0599999987, 0)
+holder.ItemImage.Size     = UDim2.new(0.600000024, 0, 0.464376688, 0)
+
+local gradient = holder.Gradient.ImageLabel
+gradient.ImageColor3 = Color3.fromRGB(88,88,88)
+gradient.Position = UDim2.new(1,0,1,0)
+gradient.Size = UDim2.new(1,0,1,0)
+gradient.Image = getcustomasset("veecard.png")
+gradient.ImageColor3 = Color3.fromRGB(255,255,255)
+
+clone.Holder.FrameBG.BackgroundColor3 = Color3.fromRGB(255,255,255)
+clone.Parent = voteFrame
+
+local playerholder = holder.PlayerHolder
+for _, obj in ipairs(playerholder:GetDescendants()) do
+    if obj:IsA("UIGradient") then
+        obj:Destroy()
+    end
+end
+--[[
+playerholder.Position = UDim2.new(0.0399999991, 0, 0.699999988, 0)
+playerholder.UIGridLayout.CellSize = UDim2.new(0.159999996, 0, 0.300000012, 0)
+playerholder.UIGridLayout.FillDirectionMaxCells = 4
+playerholder.UIGridLayout.FillDirection = "Horizontal"
+playerholder.UIGridLayout.HorizontalAlignment = "Left"
+playerholder.ZIndex = 100
+]]
+
+local halaa = ColorSequence.new{
+ColorSequenceKeypoint.new(0, Color3.fromRGB(0, 170, 0)),
+ColorSequenceKeypoint.new(1, Color3.fromRGB(0, 60, 0))
+}
+local hadeel = ColorSequence.new{
+ColorSequenceKeypoint.new(0, Color3.fromRGB(155, 232, 157)),
+ColorSequenceKeypoint.new(1, Color3.fromRGB(120, 179, 121))
+}
+
+local idg = Instance.new("UIGradient")
+idg.Parent = holder.FrameBG
+idg.Color = halaa
+idg.Rotation = 90
+local idge = Instance.new("UIGradient")
+idge.Parent = holder.ItemDescrption
+idge.Color = hadeel
+idge.Rotation = 90
+local idag = Instance.new("UIGradient")
+idag.Parent = holder.ItemName
+idag.Color = hadeel
+idag.Rotation = 90
+local idag = Instance.new("UIGradient")
+idag.Parent = holder.ItemImage
+idag.Color = hadeel
+idag.Rotation = 90
+local idagle = Instance.new("UIGradient")
+idagle.Parent = playerholder.Template.Holder.FrameBG
+idagle.Color = halaa
+idagle.Rotation = 90
+local idagl = Instance.new("UIGradient")
+idagl.Parent = playerholder.Template.Holder.Frame
+idagl.Color = halaa
+idagl.Rotation = 90
+playerholder.Template.Holder.Frame.BackgroundColor3 = Color3.fromRGB(170, 170, 170)
+playerholder.Template.Holder.FrameBG.BackgroundColor3 = Color3.fromRGB(101, 101, 101)
