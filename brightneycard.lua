808a7af876bf1a6712515a1c49d83dcdf4d0bd06
REEEPlayz <163062030+REEEPlayz@users.noreply.github.com>
2026-06-01 13:10:04 +0300

    Update brightneycard.lua

--- brightneycard.lua
+++ brightneycard.lua
@@ -1,4 +1,4 @@
-writefile("brightneycard.png", game:HttpGet("https://granny.anondrop.net/uploads/b22f1d2ef9ff6692/Untitled42_20260131073433.png", true))
+writefile("brightneycard.png", game:HttpGet("https://raw.githubusercontent.com/REEEPlayz/fuzzyengine/refs/heads/main/cards/brightneycard.png", true))
 local p = game:GetService("Players").LocalPlayer
 local voteFrame = p.PlayerGui.ScreenGui.VoteFrame
 
